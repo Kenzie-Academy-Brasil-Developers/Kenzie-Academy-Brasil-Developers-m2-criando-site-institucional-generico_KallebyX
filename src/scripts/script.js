8b6f4da7461ed77aec297e4899a620fac0efd254
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-05-30 07:19:26 -0300

    script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,21 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal() {
+    const button = document.querySelector(".openModal");
+    const modalContainer = document.querySelector("#modalContainer");
+  
+    button.addEventListener("click", () => {
+      modalContainer.showModal();
+  
+      closeModal();
+    });
+  }
+  
+  function closeModal() {
+    const button = document.querySelector("#closeModal");
+    const modalContainer = document.querySelector("#modalContainer");
+  
+    button.addEventListener("click", () => {
+      modalContainer.close();
+    });
+  }
+  
+  handleModal();
